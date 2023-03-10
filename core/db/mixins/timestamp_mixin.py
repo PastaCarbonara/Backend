809fe8f57a812e245795c52c9e905b4600f6f9cf
Tientjie <tientjieit@gmail.com>
2023-03-10 16:18:01 +0100

    default aangepast

--- core/db/mixins/timestamp_mixin.py
+++ core/db/mixins/timestamp_mixin.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(
-        default=func.now(), onupdate=func.now()
+    created_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now()
+    )
+    updated_at: Mapped[datetime | None] = mapped_column(
+        default=func.now(),
+        onupdate=func.now(),
+        server_default=func.now(),
+        server_onupdate=func.now(),
     )
