a65fa5541179109404a3588ed68d560ac42cf66b
Tientjie <tientjieit@gmail.com>
2023-03-13 14:20:33 +0100

    usernotfound exception added

--- app/recipe/services/recipe.py
+++ app/recipe/services/recipe.py
@@ -1,9 +1,10 @@
 from typing import List
 from sqlalchemy import or_, select, and_
 from sqlalchemy.orm import joinedload
-from core.db.models import RecipeJudgement, Recipe, RecipeTag
+from core.db.models import RecipeJudgement, Recipe, RecipeTag, User
 from core.db import Transactional, session
 from core.exceptions.recipe import RecipeNotFoundExeption
+from core.exceptions.user import UserNotFoundException
 
 
 class RecipeService:
@@ -12,17 +13,23 @@ class RecipeService:
 
     @Transactional()
     async def judge_recipe(self, recipe_id: int, user_id: int, like: bool) -> None:
-        recipe_query = select(Recipe).where(Recipe.id==recipe_id)
+        recipe_query = select(Recipe).where(Recipe.id == recipe_id)
         result = await session.execute(recipe_query)
         recipe = result.scalars().first()
-        if not recipe: raise RecipeNotFoundExeption
-        
+        if not recipe:
+            raise RecipeNotFoundExeption
+
+        user_query = select(User).where(User.id == user_id)
+        result = await session.execute(user_query)
+        user = result.scalars().first()
+        if not user:
+            raise UserNotFoundException
+
         exists_query = select(RecipeJudgement).where(
             (RecipeJudgement.recipe_id == recipe_id)
             & (RecipeJudgement.user_id == user_id)
         )
         result = await session.execute(exists_query)
-        
 
         judgment = result.scalars().first()
         if judgment:
