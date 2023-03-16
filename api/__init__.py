ebb9aac6fc62a17fd08e139b480ad2147e894068
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2023-03-16 11:37:55 +0100

    versioning

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 
 from api.user.v1.user import user_router as user_v1_router
-from api.auth.auth import auth_router
-from api.recipe.v1.recipe import recipe_router
+from api.auth.v1.auth import auth_v1_router
+from api.recipe.v1.recipe import recipe_v1_router
+from api.recipe.v2.recipe import recipe_v2_router
 
 router = APIRouter()
-router.include_router(user_v1_router, prefix="/api/v1/users", tags=["User"])
-router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-router.include_router(recipe_router, prefix="/api/v1/recipes", tags=["Recipe"])
+router.include_router(user_v1_router, prefix="/users", tags=["User"])
+router.include_router(auth_v1_router, prefix="/auth", tags=["Auth"])
+router.include_router(recipe_v1_router, prefix="/recipes", tags=["Recipe"])
+router.include_router(recipe_v2_router, prefix="/recipes", tags=["Recipe"])
 
 
 __all__ = ["router"]
