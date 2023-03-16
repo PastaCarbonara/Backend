267e21e5ca39dc1beda511bdfba404004eda665d
Tientjie <tientjieit@gmail.com>
2023-03-16 15:57:20 +0100

    full recipe

--- app/recipe/schemas/recipe.py
+++ app/recipe/schemas/recipe.py
@@ -2,10 +2,11 @@ from typing import List
 from pydantic import BaseModel, Field
 
 from .judgement import JudgementSchema
+
 # from .instruction import InstructionItemSchema
 # from .ingredient import GetIngredientSchema, IngredientSchema
 from .user import UserSchema
-from .tag import RecipeTagSchema
+from .tag import RecipeTagSchema, FlattendRecipeTagSchema
 
 
 class GetRecipeListResponseSchema(BaseModel):
@@ -26,11 +27,13 @@ class GetFullRecipeResponseSchema(BaseModel):
     creator: UserSchema = Field(..., description="Creator of the recipe")
     preparing_time: int = Field(..., description="Time in minutes")
 
-    tags: List[RecipeTagSchema] = Field(..., description="Tags of the recipe")
+    tags: List[FlattendRecipeTagSchema] = Field(..., description="Tags of the recipe")
     instructions: List[str] = Field(..., description="Instructions for the recipe")
     ingredients: List[str] = Field(..., description="Ingridients for the recipe")
-    judgements: List[JudgementSchema] = Field(..., description="Judgements of the recipe")
-    
+    # judgements: List[JudgementSchema] = Field(
+    #     ..., description="Judgements of the recipe"
+    # )
+
     class Config:
         orm_mode = True
 
@@ -45,8 +48,10 @@ class CreateRecipeBaseRequestSchema(BaseModel):
     instructions: List[str] = Field(..., description="Instructions for the recipe")
     ingredients: List[str] = Field(..., description="Ingridients for the recipe")
 
+
 class UserCreateRecipeRequestSchema(CreateRecipeBaseRequestSchema):
     user_id: int = None
+
 
 class CreatorCreateRecipeRequestSchema(CreateRecipeBaseRequestSchema):
     creator_id: int = None
