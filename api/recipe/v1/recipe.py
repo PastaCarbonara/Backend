353dc82ae2c08d6851a4cde92f4319ceef7e173f
Tientjie <tientjieit@gmail.com>
2023-04-04 08:55:58 +0200

    removed unused code

--- api/recipe/v1/recipe.py
+++ api/recipe/v1/recipe.py
@@ -1,17 +1,14 @@
-from typing import List, Dict, Union, Annotated
+from typing import List
 
-from fastapi import APIRouter, Depends, Query, Request, Form, File, UploadFile
-from fastapi.responses import PlainTextResponse
+from fastapi import APIRouter, Depends
 from core.exceptions import ExceptionResponseSchema
 from core.fastapi_versioning import version
 
 from app.recipe.schemas import (
     JudgeRecipeRequestSchema,
-    GetRecipeListResponseSchema,
     GetFullRecipeResponseSchema,
     UserCreateRecipeRequestSchema,
     CreatorCreateRecipeRequestSchema,
-    CreateRecipeIngredientSchema,
 )
 from app.recipe.services import RecipeService
 from core.fastapi.dependencies.permission import (
@@ -69,65 +66,3 @@ async def create_recipe(request: UserCreateRecipeRequestSchema):
         CreatorCreateRecipeRequestSchema(creator_id=request.user_id, **request.dict())
     )
     return await RecipeService().get_recipe_by_id(recipe_id)
-
-
-def form_to_schema(
-    name: Annotated[str, Form()],
-    description: Annotated[str, Form()],
-    preparing_time: Annotated[int, Form()],
-    image: Annotated[str, Form()],
-    user_id: Annotated[int, Form()],
-    tags: Annotated[List[int], Form()],
-    instructions: Annotated[List[str], Form()],
-    ingredients: Annotated[List[Dict[str, Union[int, float, str]]], Form()],
-) -> UserCreateRecipeRequestSchema:
-    return UserCreateRecipeRequestSchema(
-        name=name,
-        description=description,
-        preparing_time=preparing_time,
-        image=image,
-        user_id=user_id,
-        tags=tags,
-        instructions=instructions,
-        ingredients=ingredients,
-    )
-
-
-@recipe_v1_router.post(
-    "/test",
-    responses={"400": {"model": ExceptionResponseSchema}},
-    # dependencies=[Depends(PermissionDependency([IsAdmin]))],
-)
-@version(1)
-async def endpoint(
-    name: Annotated[str, Form()],
-    description: Annotated[str, Form()],
-    preparing_time: Annotated[int, Form()],
-    image: Annotated[str, Form()],
-    user_id: Annotated[int, Form()],
-    tags: Annotated[List[int], Form()],
-    instructions: Annotated[List[str], Form()],
-    ingredients: Annotated[List[Dict[str, Union[int, float, str]]], Form()],
-    image_file: UploadFile = File(...),
-):
-    print(name)
-    # validate recipe data
-    create_recipe_schema = UserCreateRecipeRequestSchema(
-        name=name,
-        description=description,
-        preparing_time=preparing_time,
-        image=image,
-        user_id=user_id,
-        tags=tags,
-        instructions=instructions,
-        ingredients=ingredients,
-    )
-
-    # upload image to azure and get the link of uploaded image
-
-    # save recipe data in database
-    # with open("image.jpg", "wb") as image:
-    #     image.write(image_file)
-    #     image.close()
-
-    return create_recipe_schema
