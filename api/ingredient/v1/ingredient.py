c698f9c8e4d387f72d04911d7d8fe64778366bc7
Tientjie <tientjieit@gmail.com>
2023-04-07 16:49:59 +0200

    added docstrings for api/ingredient

--- api/ingredient/v1/ingredient.py
+++ api/ingredient/v1/ingredient.py
@@ -1,3 +1,5 @@
+"""Ingredient API v1."""
+
 from typing import List
 from fastapi import APIRouter, Depends
 from core.exceptions import ExceptionResponseSchema
@@ -25,6 +27,12 @@ ingredient_v1_router = APIRouter()
 )
 @version(1)
 async def get_all_ingredients():
+    """Get all ingredients.
+
+    ## Returns
+        List[IngredientSchema]: List of all ingredients.
+    """
+
     return await IngredientService().get_ingredients()
 
 
@@ -35,7 +43,15 @@ async def get_all_ingredients():
     dependencies=[Depends(PermissionDependency([[AllowAll]]))],
 )
 @version(1)
-async def get_igrdient_by_id(ingredient_id: int):
+async def get_ingrdient_by_id(ingredient_id: int):
+    """Get an ingredient by id.
+
+    ## Parameters
+    - ingredient_id (int): The id of the ingredient to retrieve.
+
+    ## Returns
+    IngredientSchema: The ingredient with the given id.
+    """
     return await IngredientService().get_ingredient_by_id(ingredient_id)
 
 
@@ -47,6 +63,14 @@ async def get_igrdient_by_id(ingredient_id: int):
 )
 @version(1)
 async def create_ingredient(request: CreateIngredientSchema):
+    """Create a new ingredient.
+    ## Parameters
+    - request (CreateIngredientSchema): The schema representing the new ingredient.
+
+    ## Returns
+    IngredientSchema: The newly created ingredient.
+    """
+
     return await IngredientService().create_ingredient(request)
 
 
@@ -58,6 +82,15 @@ async def create_ingredient(request: CreateIngredientSchema):
 )
 @version(1)
 async def update_ingredient(ingredient_id: int, request: CreateIngredientSchema):
+    """Update an existing ingredient.
+    ## Parameters
+    - ingredient_id (int): The id of the ingredient to update.
+    - request (CreateIngredientSchema): The schema representing the updated ingredient.
+
+    ## Returns
+    IngredientSchema: The updated ingredient.
+    """
+
     return await IngredientService().update_ingredient(ingredient_id, request)
 
 
@@ -69,4 +102,11 @@ async def update_ingredient(ingredient_id: int, request: CreateIngredientSchema)
 )
 @version(1)
 async def delete_ingredient(ingredient_id: int):
+    """Delete an existing ingredient.
+    ## Parameters
+    - ingredient_id (int): The id of the ingredient to delete.
+
+    ## Returns
+        None
+    """
     return await IngredientService().delete_ingredient(ingredient_id)
