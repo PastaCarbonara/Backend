a900055c6f1daf66d8ad540f90599d8410dd0563
Tientjie <tientjieit@gmail.com>
2023-05-10 11:59:16 +0200

    added filters to users endpoint for admin

--- api/user/v1/user.py
+++ api/user/v1/user.py
@@ -1,8 +1,11 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, status
+from fastapi.responses import JSONResponse
 from app.group.schemas.group import GroupSchema
 from app.group.services.group import GroupService
+from app.filter.services.filter import FilterService
+from app.filter.schemas.filter import FilterSchema, UserCreateSchema
 from core.exceptions import ExceptionResponseSchema
 from core.fastapi.dependencies.hashid import get_path_user_id
 from core.fastapi.dependencies.permission import IsAuthenticated, IsUserOwner
@@ -54,3 +57,32 @@ async def create_user(request: CreateUserRequestSchema):
 @version(1)
 async def get_user_groups(user_id: int = Depends(get_path_user_id)):
     return await GroupService().get_groups_by_user(user_id)
+
+
+@user_v1_router.get(
+    "/{user_id}/filters",
+    response_model=list[FilterSchema],
+    dependencies=[Depends(PermissionDependency([[IsAdmin]]))]
+)
+@version(1)
+async def get_user_filters(user_id: int = Depends(get_path_user_id)):
+    return await FilterService().get_all_filters_user(user_id)
+
+
+@user_v1_router.post(
+    "/{user_id}/filters",
+    dependencies=[Depends(PermissionDependency([[IsAdmin]]))]
+)
+@version(1)
+async def create_user_filter(user_filter: UserCreateSchema, user_id: int = Depends(get_path_user_id)):
+    await FilterService().store_filters(user_id, user_filter.tags)
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "Filter created successfully."})
+
+@user_v1_router.delete("/{user_id}/filters", 
+    responses={"400": {"model": ExceptionResponseSchema}},
+    dependencies=[Depends(PermissionDependency([[IsAdmin]]))]
+    )
+@version(1)
+async def delete_user_filter(id: int, user_id: int = Depends(get_path_user_id)):
+    await FilterService().delete_filter(user_id, id)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Filter deleted successfully."})
--- app/filter/services/filter.py
+++ app/filter/services/filter.py
@@ -1,22 +1,33 @@
 from app.filter.repository.filter import FilterRepository
 from app.tag.repository.tag import TagRepository
+from app.user.repository.user import UserRepository
+from app.user.exception.user import UserNotFoundException
 from core.db import Transactional
 from core.db.models import Tag
+
 
 
 class FilterService:
     def __init__(self):
         self.filter_repository: FilterRepository = FilterRepository()
         self.tag_repository: TagRepository = TagRepository()
+        self.user_repository: UserRepository = UserRepository()
 
     @Transactional()
     async def store_filter(self, user_id: int, user_filter: int):
         """Store a filter for a user."""
+        user = await self.user_repository.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundException()
         await self.filter_repository.store_filter(user_id, user_filter)
 
     @Transactional()
     async def store_filters(self, user_id, user_filters: list[int]):
         """Store filters for a user."""
+        user = await self.user_repository.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundException()
+
         filters = await self.filter_repository.get_all_filters_user(user_id)
         tags = await self.tag_repository.get_tags()
 
@@ -28,9 +39,15 @@ class FilterService:
 
     @Transactional()
     async def delete_filter(self, user_id, tag_id):
+        user = await self.user_repository.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundException()
         await self.filter_repository.delete_filter_user(user_id, tag_id)
 
     async def get_all_filters_user(self, user_id) -> list[Tag]:
         """Get all filters for a user."""
+        user = await self.user_repository.get_user_by_id(user_id)
+        if not user:
+            raise UserNotFoundException()
         return await self.filter_repository.get_all_filters_user(user_id)
     
