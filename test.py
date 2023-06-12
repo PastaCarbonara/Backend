8a1212a6b0cf13a368e3ac18702084c534920b64
troshujin <70627990+TijmenSimons@users.noreply.github.com>
2023-06-12 14:36:19 +0200

    Update test_swipe_session.py

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 # pylint: skip-file
 
-import re
 
 
-value = (
-    'File "D:\\Users\\User\\Documents\\school\\Munchie\\Backend\\app\\swipe_session_recipe_queue\\services\\swipe_session_recipe_queue.py", line 28, in get_and_progress_queue'
-    '   for que in swipe_session_recipe_queue.queue'
-)
+x = [1 ,2 ,3 ,4 ,5]
 
-value = str(value)
-print(value)
-value = re.sub(r'[^\w\s-]', '-', value.lower())
-print(value)
-value = re.sub(r"[-\s]+", "-", value).strip("-_")
-print(value)
+print(x[1:])
