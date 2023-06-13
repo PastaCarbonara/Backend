10210265a45498e3bfb0439e6e84fbe221269ffd
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2023-06-13 14:51:11 +0200

    Update home.py

--- api/home/home.py
+++ api/home/home.py
@@ -4,9 +4,6 @@ from core.fastapi.dependencies import PermissionDependency, AllowAll
 
 home_router = APIRouter()
 
-
-XXX = 2
-
 @home_router.get("/health", dependencies=[Depends(PermissionDependency([[AllowAll]]))])
 async def home():
     return Response(status_code=200)
