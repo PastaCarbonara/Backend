dda3459f8088f3799c547a700c304e32aafe8d65
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2023-06-29 15:55:46 +0200

    Update home.py

--- api/home/home.py
+++ api/home/home.py
@@ -9,14 +9,3 @@ home_router = APIRouter()
 @home_router.get("/health", dependencies=[Depends(PermissionDependency([[AllowAll]]))])
 async def home():
     return Response(status_code=200)
-
-
-@home_router.get("/cheat", dependencies=[Depends(PermissionDependency([[IsAdmin]]))])
-async def cheat():
-    result = {}
-
-    for key, item in manager.active_pools.items():
-        item["connections"] = ["WebSocket" for _ in item["connections"]]
-        result[key] = item
-
-    return result
